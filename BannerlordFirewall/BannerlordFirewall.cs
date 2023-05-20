96ab0678e421c0e5965c7089e31144be491433f8
Berat Yaman <mrbh228@gmail.com>
2023-05-20 19:49:50 +0300

    Initial commit

---
+++ BannerlordFirewall/BannerlordFirewall.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.MountAndBlade;
+
+namespace BannerlordFirewall
+{
+    public class BannerlordFirewall : MBSubModuleBase
+    {
+        protected HarmonyLib.Harmony HarmonyHandle;
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            this.HarmonyHandle = new HarmonyLib.Harmony("mentalrob.bannerlordfirewall.bannerlord");
+            
+            
+
+        }
+    }
+}
